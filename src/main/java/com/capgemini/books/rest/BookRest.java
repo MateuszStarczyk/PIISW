4e4b72831b2453c37d55141ab069081727b2ec44
Mateusz.Starczyk <mati.starczyk@gmail.com>
2020-03-09 08:29:41 +0100

    commit3

--- src/main/java/com/capgemini/books/rest/BookRest.java
+++ src/main/java/com/capgemini/books/rest/BookRest.java
@@ -1,5 +1,5 @@
 package com.capgemini.books.rest;
-
+//commit3
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
