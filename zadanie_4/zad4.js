eb1e2acd579047719e3dbef1d595819abaed0e98
Mateusz.Starczyk <mati.starczyk@gmail.com>
2020-03-27 15:21:19 +0100

    fix zad4

--- zadanie_4/zad4.js
+++ zadanie_4/zad4.js
@@ -1,5 +1,5 @@
-var accountMaker = function(spec) {
-    var balance = 0,
+var accountMaker = function (spec) {
+    const balance = 0,
         number = spec.number,
         creationDate = spec.creationDate,
         currency = spec.currency;
@@ -21,13 +21,13 @@ var accountMaker = function(spec) {
             this.balance += amount;
             return this.balance;
         },
-        show: function() { console.log('balance: ' + balance + ' number: ' + number
-            + ' creationDate: ' + creationDate + ' currency: ' + currency); },
-        account: {
-            balance: balance,
-            number: number,
-            creationDate: creationDate,
-            currency: currency
+        show: function () {
+            return {
+                balance,
+                number,
+                creationDate,
+                currency
+            }
         }
 
     };
@@ -35,5 +35,4 @@ var accountMaker = function(spec) {
 
 var account = accountMaker({number: 12345678901, currency: 'PLN', creationDate: Date.now()});
 var account2 = accountMaker({number: 12345678901, currency: 'PLN', creationDate: Date.now()});
-account2.show();
-console.log(account.account);
+console.log(account.show());
